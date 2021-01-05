58fa8dc9a0d2401d9565d39bd2e3aa1c89eb7276
CXY0501 <chen.xy.2@foxmail.com>
2021-01-05 22:45:20 +0800

    2020010504

--- app2.js
+++ app2.js
@@ -1,21 +1,26 @@
-var http = require('http');
-var fs = require('fs')
+const http = require('http');
+const fs = require('fs')
+const common = require('./module/common.js')
+const path = require('path')
+var url = require ('url')
 http.createServer(function (request, response) {
-    var path=request.url;
-    path=path=='/'?'/index.html':path;
+    var pathname=request.url;
+    pathname=pathname=='/'?'/index.html':pathname;
+    let extname = path.extname(pathname);
+    // console.log(extname)
     if(path!='/favicon.ico'){
-        fs.readFile('./html'+path,(err,data)=>{
+        fs.readFile('./html'+pathname,(err,data)=>{
             if(err){
                 response.writeHead(404, {'Content-Type': 'text/html;charset="utf-8"'});
                 response.end('404页面不存在');
             }else{
-                response.writeHead(200, {'Content-Type': 'text/html;charset="utf-8"'});
+                let mime=common.getMime(extname)
+                response.writeHead(200, {'Content-Type': ''+mime+';charset="utf-8"'});
+                console.log(data.toString())
                 response.end(data);
             }
         })
     }
-
-  
 }).listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000/');
