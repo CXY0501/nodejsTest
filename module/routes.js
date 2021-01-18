e5f9ae3249889a3acf1b36279f8d1379c90078d1
CXY0501 <chen.xy.2@foxmail.com>
2021-01-18 23:50:08 +0800

    2021011801

--- module/routes.js
+++ module/routes.js
@@ -3,26 +3,35 @@ const path = require('path')
 var url = require ('url')
 
 let getFileMime = function(extname){
-    var data = fs.readFileSync('./static/mime.json')
+    let data = fs.readFileSync('./static/mime.json')
     let mimeObj = JSON.parse(data.toString());
     return mimeObj[extname];
     // console.log(data); 
 }
 
 exports.static = function(req,res,staticPath){
-    var pathname=url.parse(req.url).pathname;
+    let pathname=url.parse(req.url).pathname;
     pathname=pathname=='/'?'/index.html':pathname;
     let extname = path.extname(pathname);
-    if(path!='/favicon.ico'){
-        fs.readFile('./'+staticPath+pathname,(err,data)=>{
-            if(err){
-                res.writeHead(404, {'Content-Type': 'text/html;charset="utf-8"'});
-                res.end('404页面不存在');
-            }else{
-                let mime=getFileMime(extname)
+    if(pathname!='/favicon.ico'){
+        // fs.readFile('./'+staticPath+pathname,(err,data)=>{
+        //     if(err){
+        //         res.writeHead(404, {'Content-Type': 'text/html;charset="utf-8"'});
+        //         res.end('404页面不存在');
+        //     }else{
+        //         let mime=getFileMime(extname)
+        //         res.writeHead(200, {'Content-Type': ''+mime+';charset="utf-8"'});
+        //         res.end(data);
+        //     }
+        // })
+        try{
+            let data=fs.readFileSync('./' + staticPath + pathname);
+            // console.log(data);
+            if(data){
+                let mime=getFileMime(extname);
                 res.writeHead(200, {'Content-Type': ''+mime+';charset="utf-8"'});
                 res.end(data);
-            }
-        })
-    }
+            }     
+        }catch(error){}
+        }  
 }
